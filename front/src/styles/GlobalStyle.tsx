b2d820ce6c4235610a9c18993e5ad09ff7f8dac1
Soohyun Jung <sasha1107@naver.com>
2023-02-26 01:27:35 +0900

    config global style

---
+++ front/src/styles/GlobalStyle.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import TheJamsilThin from "@assets/fonts/The-Jamsil-1-Thin.woff";
+import TheJamsilLight from "@assets/fonts/The-Jamsil-2-Light.woff";
+import TheJamsilRegular from "@assets/fonts/The-Jamsil-3-Regular.woff";
+import TheJamsilMedium from "@assets/fonts/The-Jamsil-4-Medium.woff";
+import TheJamsilBold from "@assets/fonts/The-Jamsil-5-Bold.woff";
+import TheJamsilExtraBold from "@assets/fonts/The-Jamsil-6-ExtraBold.woff";
+import BlackHanSans from "@assets/fonts/BlackHanSans-Regular.woff";
+
+export const GlobalStyle = createGlobalStyle`
+    ${reset}
+
+    @font-face {
+        font-family: "theJamsil";
+        src: local("theJamsil"), url(${TheJamsilThin}) format('woff'); 
+        font-weight: 100;
+    }
+
+    @font-face {
+        font-family: "theJamsil";
+        src: local("theJamsil"), url(${TheJamsilLight}) format('woff'); 
+        font-weight: 300;
+    }
+
+    @font-face {
+        font-family: "theJamsil";
+        src: local("theJamsil"), url(${TheJamsilRegular}) format('woff'); 
+        font-weight: 400;
+    }
+
+    @font-face {
+        font-family: "theJamsil";
+        src: local("theJamsil"), url(${TheJamsilMedium}) format('woff'); 
+        font-weight: 500;
+    }
+
+    @font-face {
+        font-family: "theJamsil";
+        src: local("theJamsil"), url(${TheJamsilBold}) format('woff'); 
+        font-weight: 700;
+    }
+
+    @font-face {
+        font-family: "theJamsil";
+        src: local("theJamsil"), url(${TheJamsilExtraBold}) format('woff'); 
+        font-weight: 800;
+    }
+
+    @font-face {
+        font-family: "blackHanSans";
+        src: local("blackHanSans"), url(${BlackHanSans}) format('woff'); 
+        font-weight: normal;
+    }
+
+    :root {
+        font-family: "theJamsil";
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    input, button {
+        background-color: transparent;
+        border: none;
+        outline: none;
+    }
+
+    button {
+        cursor: pointer;
+    }
+    ol, ul, li {
+        list-style: none;
+    }
+`;
+// * {
+//     box-sizing: border-box;
+//   }
+// body{
+//     background-color: #ffffff;
+//     font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif; //여기서 쓰시는 폰트 설정하시면 될것같습니다.
+//   }
+//  h1, h2, h3, h4, h5, h6{
+//     font-family:'Maven Pro', sans-serif;
+//   }
+//   img {
+//     display: block;
+//     width: 100%;
+//     height: 100%;
+//   }
+//   } 인터넷에서 주워왓어용
