4accd94c21504ed1327f748b706a1a455366793a
Soohyun Jung <sasha1107@naver.com>
2023-02-26 15:15:30 +0900

    feat: #3 QuizForm 마크업 구현

---
+++ front/src/pages/QuizForm.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function QuizForm() {
+  return (
+    <div>
+      <h1>MBTI 명탐정</h1>
+      <h2>Q1</h2>
+      <p>1/4</p>
+      <p>오랜만에 열린 동창회, 친구는 동창회에 와서 나에게</p>
+      <div className="buttonCont">
+        <button>나를 포함해 여러 친구들에게 인사한다.</button>
+        <button>최대한 조용하게 나에게 인사한다.</button>
+      </div>
+    </div>
+  );
+}
