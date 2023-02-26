e929d348dd23278acb91a070aad0c52dd78368b3
Gyuri092 <ggr0927@gmail.com>
2023-02-26 21:55:17 +0900

    style: container 공통 스타일 컴포넌트 생성

---
+++ front/src/components/common/Container.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledBackgroundBox = styled.div`
+  background-color: #dcbc8c;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledContainBox = styled.div`
+  width: 430px;
+  height: 844px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledTitleContainBox = styled.div`
+  width: 390px;
+  height: 220px;
+  position: relative;
+`;
