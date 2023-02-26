7a211dfa7c373ffe607cdd405dbbe6c5ee406558
Soohyun Jung <sasha1107@naver.com>
2023-02-26 18:20:04 +0900

    feat: #3 QuizForm 질문 랜덤으로 받아오
    - assets/data 폴더에 question.json 파일 생성(질문리스트)
    - useMakeRandomQuestion 커스텀 훅 생성
        - question.json 파일에서 타입 별 질문을 1개씩 랜덤하게 뽑아오는 커스텀 훅
    - QuizForm.tsx에서 useMakeRandomQuestion hook으로 질문 랜덤하게 임포트

--- front/src/pages/QuizForm.tsx
+++ front/src/pages/QuizForm.tsx
@@ -1,39 +1,20 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { useMakeRandomQuestion } from "@hooks/useMakeRandomQuestion";
 import iconTarget from "@assets/images/icon/icon-target.png";
 
 export default function QuizForm() {
+  const [questions, answerText] = useMakeRandomQuestion();
+  const questionType = ["IE", "SN", "TF", "JP"];
   const [status, setStatus] = useState<number>(0);
   const [answer, setAnswer] = useState<string[]>([]);
-  const questionType = ["IE", "SN", "TF", "JP"];
-  const [question, setQuestion] = useState<string[]>([
-    "오랜만에 열린 동창회, 친구는 동창회에 와서 나에게",
-    "친구와 새벽에 술을 마시는데 친구에게 갑자기 만약에 여기에 귀신이 있다면 어떨것 같냐고 묻는다면?",
-    "내가 교통사고가 나서 병문안을 가게 됐다. 친구의 가장 첫 질문은?",
-    "여느때와 같이 평화로운 주말 오후, 갑자기 울린 전화를 받으니 나는 오늘 영화보러 가자고 했다. 친구의 대답은?",
-  ]);
-  const [answerText, setAnswerText] = useState<string[][]>([
-    [
-      "최대한 조용하게 나에게 인사한다.",
-      "나를 포함해 여러 친구들에게 인사한다.",
-    ],
-    [
-      "내 말에 관심이 없고 그냥 안주나 먹는다.",
-      "너무 무서우니 더이상 말하지 말라고 화낸다.",
-    ],
-    [
-      "어쩌다 그랬어? 상대방은 보험처리 한거래?",
-      "헉, 너무 아프겠다... 내가 과일사왔는데 좀 먹어.",
-    ],
-    ["미안한데 나 오늘은 쉬고싶어.", "너무 좋아, 무슨 영화 볼까?"],
-  ]);
 
   const handleNextQuestion = (e: React.MouseEvent<HTMLButtonElement>) => {
     setAnswer([...answer, (e.target as HTMLButtonElement).value]);
+    if (status === 3) {
+      // api 호출(결과 전송)
+    }
     setStatus((prev) => prev + 1);
-    // if (status < 3) {
-    //   setStatus((prev) => prev + 1);
-    // }
   };
 
   return (
@@ -45,11 +26,11 @@ export default function QuizForm() {
           <StyledTargetImage src={iconTarget} />
         </StyledStatusBarBox>
       </StyledStatusBox>
-      {status < 4 ? (
+      {status < 4 && questions && answerText ? (
         <StyledQuestionContainer>
           <StyledQuestionTitle>Q{status + 1}</StyledQuestionTitle>
           <StyledStatusSpan>({status + 1}/4)</StyledStatusSpan>
-          <StyledQuestionBox>{question[status]}</StyledQuestionBox>
+          <StyledQuestionBox>{questions[status]}</StyledQuestionBox>
           <StyledButtonsBox>
             <StyledButton
               className="top"
@@ -86,6 +67,7 @@ const StyledStatusBox = styled.div`
   border: 2px solid black;
   margin: 20px 0 40px;
 `;
+
 const StyledStatusBarBox = styled.div<{ width: number }>`
   width: ${(props) => props.width}%;
   height: 20px;
@@ -93,6 +75,7 @@ const StyledStatusBarBox = styled.div<{ width: number }>`
   position: relative;
   transition: 0.5s;
 `;
+
 const StyledTargetImage = styled.img`
   width: 36px;
   height: 36px;
@@ -100,9 +83,11 @@ const StyledTargetImage = styled.img`
   right: -18px;
   bottom: -8px;
 `;
+
 const StyledQuestionContainer = styled.section`
   position: relative;
 `;
+
 const StyledQuestionTitle = styled.h2`
   display: block;
   font-size: 36px;
