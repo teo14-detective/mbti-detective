6dffb0703b2b825041e0f943c9b4c2fd4b06b9c7
Jxxunnn <roof2586@naver.com>
2023-02-27 01:37:35 +0900

    feat chart, compare 경로 추가   #1
    Co-authored-by: devep441 <Bsfla@users.noreply.github.com>

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ResultPage from "@pages/ResultPage";
+import ChartResultPage from "@pages/ChartResultPage";
+import CompareResultPage from "@pages/CompareResultPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<MainPage />} /> */}
-        <Route path="/" element={<ResultPage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="/result/chart" element={<ChartResultPage />} />
+        <Route path="/result/compare" element={<CompareResultPage />} />
       </Routes>
     </BrowserRouter>
   );
