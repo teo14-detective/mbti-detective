2e790e6aebff2cbef9f148f2b77d481e44a66831
Gyuri092 <ggr0927@gmail.com>
2023-02-26 22:42:45 +0900

    style: container.tsx 스타일 추가

--- front/src/components/common/Container.tsx
+++ front/src/components/common/Container.tsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 
 export const StyledBackgroundBox = styled.div`
   background-color: #dcbc8c;
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
 
 export const StyledContainBox = styled.div`
@@ -14,10 +15,35 @@ export const StyledContainBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-around;
 `;
 
 export const StyledTitleContainBox = styled.div`
   width: 390px;
   height: 220px;
   position: relative;
+`;
+
+export const StyledSnsContainerBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const StyledSnsContainerCircleBox = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 8px;
+  gap: 10px;
+  width: 38px;
+  height: 38px;
+  background: #8f754d;
+  border-radius: 30px;
+  margin-left: 9px;
+`;
+
+export const StyledMBTIButtonContainerBox = styled.div`
+  display: flex;
+  justify-content: center;
 `;
