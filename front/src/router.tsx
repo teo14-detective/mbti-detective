302a0c468f6fea8dd335cdcd075aaa25a435bd16
Jxxunnn <roof2586@naver.com>
2023-02-26 18:00:45 +0900

    $ git commit -m "feat 라우터 설정 #1" > > Co-authored-by: jxxunnn <roof2586@naver.com> Co-authored-by: Bsfla <baebeomsu77@gmail.com>"

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ResultPage from "@pages/ResultPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path="/" element={<MainPage />} /> */}</Routes>
+      <Routes>
+        {/* <Route path="/" element={<MainPage />} /> */}
+        <Route path="/" element={<ResultPage />} />
+      </Routes>
     </BrowserRouter>
   );
 };
