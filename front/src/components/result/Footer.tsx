7e52e501f29ecbbfc14b36bc51b3b4b5a0abe5ca
Jxxunnn <roof2586@naver.com>
2023-02-26 21:17:04 +0900

    카카오톡 공유 기능 구현 #1
    Co-authored-by: 정수현 <sasha1107@users.noreply.github.com>

--- front/src/components/result/Footer.tsx
+++ front/src/components/result/Footer.tsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import kakaoIcon from "@assets/images/icon/icon-kakao.png";
 import linkIcon from "@assets/images/icon/icon-link.png";
 import saveIcon from "@assets/images/icon/icon-save.png";
+
+const KakaoShareButton = () => {
+  useEffect(() => {
+    createKakaoButton();
+  }, []);
+
+  const createKakaoButton = () => {
+    // kakao sdk script이 정상적으로 불러와졌으면 window.Kakao로 접근이 가능합니다
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+
+      // 중복 initialization 방지
+      if (!kakao.isInitialized()) {
+        // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+        kakao.init(import.meta.env.VITE_KAKAO_KEY);
+      }
+
+      kakao.Link.createDefaultButton({
+        // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
+        container: "#kakao-link-btn",
+        objectType: "feed",
+        content: {
+          title: "타이틀",
+          description: "#나는야 #MBTI #명탐정",
+          imageUrl:
+            "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/78e62aef-f8dd-4a6f-8c0f-ecae5737813c/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230226%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230226T114336Z&X-Amz-Expires=86400&X-Amz-Signature=4695abcd0c95d1638e7a552142682b377855e0fc2e7f0825a4dd9b160e709bc7&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject", // i.e. process.env.FETCH_URL + '/logo.png'
+          link: {
+            mobileWebUrl: window.location.href,
+            webUrl: window.location.href,
+          },
+        },
+        /* social은 optional */
+        social: {
+          likeCount: 77,
+          commentCount: 55,
+          sharedCount: 333,
+        },
+        buttons: [
+          {
+            title: "웹으로 보기",
+            link: {
+              mobileWebUrl: window.location.href,
+              webUrl: window.location.href,
+            },
+          },
+        ],
+      });
+    }
+  };
+  return (
+    <StyledButton id="kakao-link-btn" type="button">
+      <StyledImage src={kakaoIcon} alt="카카오톡" />
+    </StyledButton>
+  );
+};
 
 const Footer = () => {
   return (
@@ -18,9 +73,7 @@ const Footer = () => {
         </StyledDataList>
       </StyledContainer>
       <StyledButtonContainer>
-        <StyledButton type="button">
-          <StyledImage src={kakaoIcon} alt="카카오톡" />
-        </StyledButton>
+        <KakaoShareButton />
         <StyledButton type="button">
           <StyledImage src={linkIcon} alt="링크" />
         </StyledButton>
@@ -33,6 +86,8 @@ const Footer = () => {
 };
 
 export default Footer;
+
+/* Footer Style */
 
 const StyledFooter = styled.footer``;
 
