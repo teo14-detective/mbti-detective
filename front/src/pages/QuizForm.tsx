f37326280846dda3d677d2d21401b4c12ff43d18
Soohyun Jung <sasha1107@naver.com>
2023-02-26 16:43:10 +0900

    style: #3 QuizForm 스타일링 구현
    - Button, Header 추후 컴포넌트로 교체 필요

--- front/src/pages/QuizForm.tsx
+++ front/src/pages/QuizForm.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+import iconTarget from "@assets/images/icon/icon-target.png";
 
 export default function QuizForm() {
   const [status, setStatus] = useState<number>(0);
@@ -28,32 +30,120 @@ export default function QuizForm() {
 
   const handleNextQuestion = (e: React.MouseEvent<HTMLButtonElement>) => {
     setAnswer([...answer, (e.target as HTMLButtonElement).value]);
-    if (status < 3) {
-      setStatus((prev) => prev + 1);
-    }
+    setStatus((prev) => prev + 1);
+    // if (status < 3) {
+    //   setStatus((prev) => prev + 1);
+    // }
   };
 
   return (
-    <div>
+    <StyledContainer>
+      <button>뒤로가기</button>
       <h1>MBTI 명탐정</h1>
-      <h2>Q{status + 1}</h2>
-      <p>{status + 1}/4</p>
-      <p>{question[status]}</p>
-      <div className="buttonCont">
-        <button
-          onClick={handleNextQuestion}
-          value={questionType[status].split("")[0]}
-        >
-          {answerText[status][0]}
-        </button>
-        <button
-          onClick={handleNextQuestion}
-          value={questionType[status].split("")[1]}
-        >
-          {answerText[status][1]}
-        </button>
-      </div>
-      <div>{...answer}</div>
-    </div>
+      <StyledStatusBox>
+        <StyledStatusBarBox width={status * 25}>
+          <StyledTargetImage src={iconTarget} />
+        </StyledStatusBarBox>
+      </StyledStatusBox>
+      {status < 4 ? (
+        <StyledQuestionContainer>
+          <StyledQuestionTitle>Q{status + 1}</StyledQuestionTitle>
+          <StyledStatusSpan>({status + 1}/4)</StyledStatusSpan>
+          <StyledQuestionBox>{question[status]}</StyledQuestionBox>
+          <StyledButtonsBox>
+            <StyledButton
+              className="top"
+              onClick={handleNextQuestion}
+              value={questionType[status].split("")[0]}
+            >
+              {answerText[status][0]}
+            </StyledButton>
+            <StyledButton
+              className="bottom"
+              onClick={handleNextQuestion}
+              value={questionType[status].split("")[1]}
+            >
+              {answerText[status][1]}
+            </StyledButton>
+          </StyledButtonsBox>
+        </StyledQuestionContainer>
+      ) : (
+        <></>
+      )}
+      <div>결과:{...answer}</div>
+    </StyledContainer>
   );
 }
+const StyledContainer = styled.section`
+  max-width: 430px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const StyledStatusBox = styled.div`
+  width: 100%;
+  height: 20px;
+  border: 2px solid black;
+  margin: 20px 0 40px;
+`;
+const StyledStatusBarBox = styled.div<{ width: number }>`
+  width: ${(props) => props.width}%;
+  height: 20px;
+  background-color: #9e2311;
+  position: relative;
+  transition: 0.5s;
+`;
+const StyledTargetImage = styled.img`
+  width: 36px;
+  height: 36px;
+  position: absolute;
+  right: -18px;
+  bottom: -8px;
+`;
+const StyledQuestionContainer = styled.section`
+  position: relative;
+`;
+const StyledQuestionTitle = styled.h2`
+  display: block;
+  font-size: 36px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+
+const StyledStatusSpan = styled.span`
+  display: block;
+  font-style: 20px;
+  color: #554128;
+  margin-bottom: 80px;
+`;
+const StyledQuestionBox = styled.div`
+  font-size: 24px;
+  width: 80%;
+  margin: 0 auto;
+`;
+const StyledButtonsBox = styled.div`
+  max-width: 430px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 10%;
+  width: 80%;
+  gap: 40px;
+`;
+
+const StyledButton = styled.button`
+  width: 100%;
+  height: 80px;
+  padding: 20px;
+  font-family: "theJamsil";
+  font-size: 20px;
+  &.top {
+    background-color: #f498b7;
+  }
+  &.bottom {
+    background-color: #01e3e0;
+  }
+`;
