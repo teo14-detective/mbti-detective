91e7fadbb0e3dcac6df303ad2863a5bb6dd12c00
FROM-minyoung <918liter@gmail.com>
2023-02-26 16:00:40 +0900

    style: #6 display flex 적용

--- front/src/pages/main.tsx
+++ front/src/pages/main.tsx
@@ -6,26 +6,32 @@ export default function MainPage() {
     <Container>
       <img src="src/assets/images/logo.png" alt="로고" />
       <div>슬라이드</div>
-      <label>이름을 입력해주세요</label>
-      <input type="text" />
+      <StyledFlexGrid>
+        <label>이름을 입력해주세요</label>
+        <input type="text" />
+      </StyledFlexGrid>
       <label>MBTI는 무엇인가요?</label>
-      <StyledButtonGrid>
+      <StyledFlexGrid>
         <StyledMBTIButton>I</StyledMBTIButton>
         <StyledMBTIButton>N</StyledMBTIButton>
         <StyledMBTIButton>F</StyledMBTIButton>
         <StyledMBTIButton>P</StyledMBTIButton>
-      </StyledButtonGrid>
-      <StyledButtonGrid>
+      </StyledFlexGrid>
+      <StyledFlexGrid>
         <StyledMBTIButton>E</StyledMBTIButton>
         <StyledMBTIButton>S</StyledMBTIButton>
         <StyledMBTIButton>T</StyledMBTIButton>
         <StyledMBTIButton>J</StyledMBTIButton>
-      </StyledButtonGrid>
-      <button>start</button>
+      </StyledFlexGrid>
+      <StyledFlexGrid>
+        <button>start</button>
+      </StyledFlexGrid>
       <label>방문수</label> <label>0</label>
       <label>공유수</label> <label>0</label>
-      <button>카카오톡</button>
-      <button>링크</button>
+      <StyledFlexGrid>
+        <button>카카오톡</button>
+        <button>링크</button>
+      </StyledFlexGrid>
     </Container>
   );
 }
@@ -36,11 +42,12 @@ const Container = styled.div`
   max-width: 430px;
 `;
 
-const StyledButtonGrid = styled.div`
+const StyledFlexGrid = styled.div`
   display: flex;
 `;
 
 const StyledMBTIButton = styled.button`
+  margin: 4px;
   width: 50px;
   height: 50px;
 
