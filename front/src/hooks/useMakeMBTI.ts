2c4cf6b741d8f9872a5be76a6b73fc8ca365af44
FROM-minyoung <918liter@gmail.com>
2023-02-28 14:40:38 +0900

    fix: 헤더 css 수정, useMakeMBTI 타입 일부 수정

--- front/src/hooks/useMakeMBTI.ts
+++ front/src/hooks/useMakeMBTI.ts
@@ -1,13 +1,18 @@
 import { useState } from "react";
 
 export default function useMakeMBTI() {
-  const [MBTIResultArray, setMBTIResultArray] = useState<any>(["", "", "", ""]);
+  const [MBTIResultArray, setMBTIResultArray] = useState<any[]>([
+    "",
+    "",
+    "",
+    "",
+  ]);
 
   function clickMBTIButton(e: React.MouseEvent<HTMLButtonElement>) {
     const id = Number(e.currentTarget.id);
 
     setMBTIResultArray(
-      MBTIResultArray.map((mbti: any, index: number) => {
+      MBTIResultArray.map((mbti: string, index: number) => {
         id === index
           ? MBTIResultArray.splice(index, 1, e.currentTarget.value)
           : mbti;
@@ -17,7 +22,7 @@ export default function useMakeMBTI() {
     // id와 맞는 인덱스에 문자가 있다면, 클릭한 문자로 인덱스에 맞는 mbti 성향 바꿔주기
     if (MBTIResultArray[id]) {
       setMBTIResultArray(
-        MBTIResultArray.map((mbti: any, index: number) =>
+        MBTIResultArray.map((mbti: string, index: number) =>
           id === index ? e.currentTarget.value : mbti,
         ),
       );
