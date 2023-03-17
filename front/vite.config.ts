7be9f9dacb4b6916290aef1e12c139402ba44126
ted lee <persisne@gmail.com>
2023-03-17 23:29:36 +0900

    change api url to fly.io

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   resolve: {
     alias: [
-      { find: "@components", replacement: "/src/components" },
-      { find: "@", replacement: "/src" },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@', replacement: '/src' },
     ],
   },
   define: {
-    "process.env": {},
+    'process.env': {},
   },
   server: {
     proxy: {
-      "/api": {
-        target: "https://mbti-detective-api.netlify.app",
+      '/api': {
+        target: 'https://mbti-detective-api.fly.dev/',
         changeOrigin: true,
         secure: false,
       },
